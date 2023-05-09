247dd2f74e8332d310f35022876cabf47b4a08c3
Sabah Raad <raad@aamarpay.net>
2023-05-09 17:55:18 +0600

    parcel & profile controller added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,28 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function show(){
-        return view('dashboard');
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
+
+    public function show(){
+
+        return view('dashboard');
+
+    }
+
+    public function viewProfile(){
+
+        return view('viewProfile');
+
+    }
+
+    public function bookParcel(){
+
+        return view('bookParcel');
+
+    }
+
+    
 }
