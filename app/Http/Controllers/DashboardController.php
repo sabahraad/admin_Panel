a633a3af93331ea6205aa5f99f869e1b0530f102
Sabah Raad <raad@aamarpay.net>
2023-05-10 15:56:46 +0600

    withdraw method added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\withdrawMoney;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -29,5 +30,35 @@ class DashboardController extends Controller
 
     }
 
+    public function Withdraw(){
+
+        $amount=30000;
+
+        return view('Withdraw',compact('amount'));
+
+    }
+
+    public function WithdrawStore(Request $request){
+
+        $transectionStatus = "Pending";
+
+        $data = new withdrawMoney;
+        $data->withdrawAmount=$request->withdrawAmount;
+        $data->withdrawDateTime=$request->withdrawDateTime;
+        $data->withdrawBy =$request->withdrawBy;
+        $data->transectionStatus =$transectionStatus;
+
+        $data->save();
+
+        return redirect()->route('WithdrawList');
+
+    }
+
+    public function WithdrawList(){
+
+        $data = withdrawMoney::all();
+
+        return view('WithdrawList',compact('data'));
+    }
     
 }
